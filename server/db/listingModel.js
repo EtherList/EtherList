309fe017f44849e94a1ebe82e2d5b466e11fe557
Matt <08martinm@gmail.com>
2016-08-11 12:33:30 -0700

    feat: updated database

---
+++ server/db/listingModel.js
@@ -0,0 +1,51 @@
+var db = require('./db');
+var Sequelize = require('sequelize');
+var bluebird = require('bluebird');
+
+exports.Listings = db.define('listings', {
+
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+
+  description: {
+    type: Sequelize.STRING,
+    allowNull: true
+  },
+
+  location: {
+    type: Sequelize.STRING,
+    allowNull: true
+  },
+
+  time: {
+    type: Sequelize.STRING,
+    allowNull: true
+  }, 
+
+  reputation: {
+    type: Sequelize.STRING,
+    allowNull: true
+  },
+
+  price: {
+    type: Sequelize.STRING,
+    allowNull: true
+  },
+
+  terms: {
+    type: Sequelize.STRING,
+    allowNull: true
+  },
+
+  lat: {
+    type: Sequelize.FLOAT,
+    allowNull: true
+  },
+
+  lng: {
+    type: Sequelize.FLOAT,
+    allowNull: true
+  }
+});
