5e425a281cbad96d0a17539fc7e7d08dfc242a8a
donovanCarreno <donovan_carreno@yahoo.com>
2016-08-13 16:31:14 -0700

    chore: add copy-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,34 @@
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+
 var config = {
    entry: './client/src/App.jsx',
-  
+
    output: {
       path:'./client/public',
       filename: 'bundle.js',
    },
-  
+
    module: {
       loaders: [
          {
             test: /\.jsx?$/,
             exclude: /node_modules/,
             loader: 'babel',
-        
+
             query: {
                presets: ['es2015', 'react']
             }
          }
       ]
    },
-   devtool: 'source-map'
+   devtool: 'source-map',
+
+   plugins: [
+     new CopyWebpackPlugin([
+       {from: 'node_modules/etherrep/build/contracts', to: 'client/public'},
+       {from: 'node_modules/etherrep/build/app.js', to: 'client/public'}
+     ])
+   ]
 }
 
 module.exports = config;
