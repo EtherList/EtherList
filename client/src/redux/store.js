0cef2a5e67e64d979d272646df5c1112425182de
Nat Grigorovskaya <grigorovskaya@gmail.com>
2016-08-16 16:46:43 -0700

    (refactor) add store

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,24 +1,4 @@
-var { createStore } = require('redux');
-import { combineReducers } from 'redux'
-import * as reducers from './reducers';
+const { createStore } = require('redux');
+import reducer from './reducers';
 
-
-
-const todoApp = combineReducers({
-  visibilityFilter,
-  todos
-})
-
-export default todoApp
-
-
-// let store = createStore(todos, [ 'Use Redux' ])
-
-// store.dispatch({
-//   type: 'GET_CATEGORIES',
-//   categories: 
-// })
-
-
-
-// console.log(store.getState())
+export const store = createStore(reducer);
