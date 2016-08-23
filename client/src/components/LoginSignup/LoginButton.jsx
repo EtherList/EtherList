4351210aa3fb72efac674badf57586abd6d4ca1d
Casper M. H. Holmgreen <cholmgreen@gmail.com>
2016-08-23 11:24:23 -0700

    fix: Modify Login button to display FB logo and change to logout when signed in

--- client/src/components/LoginSignup/LoginButton.jsx
+++ client/src/components/LoginSignup/LoginButton.jsx
@@ -1,25 +1,31 @@
 import React from 'react';
-import {Link} from 'react-router';
 
 export default class LoginButton extends React.Component {
   constructor(props) {
     super(props);
   }
 
-  handleSubmit() {
+  attemptLogin() {
     //forcing the page to reload in order to make a server-side call to FB for authentication
     this.props.onLoginAttempt();
     window.location = window.location.origin + "/auth/facebook";
   }
 
+  logout() {
+    this.props.onLogout();
+    window.location = window.location.origin + "/logout";
+  }
 
   render() {
+    let action = this.props.user ? () => {this.logout()} : () => {this.attemptLogin()};
     return (
-              <div>
-                <button className='signIn btn btn-success' type='button' onClick={() => {this.handleSubmit()}}>
-                  Log In
-                </button>
-              </div>
+      <div>
+        <button className='signIn btn btn-info' type='button' onClick={action}>
+          { this.props.user ? '' : <img src="https://upload.wikimedia.org/wikipedia/commons/c/c2/F_icon.svg" style={{height: "1em"}} /> }
+          &nbsp;
+          { this.props.user ? 'Log out' : 'Log in' }
+        </button>
+      </div>
     )
   }
 }
